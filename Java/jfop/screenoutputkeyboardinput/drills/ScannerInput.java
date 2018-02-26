350ba97000fb9666ae65b245a97b7ed2fcd0b77b
Rebecca Allen <rebeccazarsky@gmail.com>
2018-02-26 15:07:23 -0700

    Add drills to ScreenOutputKeyboardInput

--- Java/jfop/screenoutputkeyboardinput/drills/ScannerInput.java
+++ Java/jfop/screenoutputkeyboardinput/drills/ScannerInput.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
 
-public class ScannerInput {
-
-	public static void main(String[] args) {
+public class ScannerInput
+{
+	public static void main(String[] args)
+	{
 		// Add a prompt to the code so that the user knows to enter data. Output the data.
+
 		int yearBorn;
-		
-		java.util.Scanner keyboard = new java.util.Scanner(System.in);
-		
+
+		Scanner keyboard = new Scanner(System.in);
+
 		// add a prompt...
-		
+		System.out.println("What year were you born? ");
 		yearBorn = keyboard.nextInt();
-		
+
 		// output the data...
-		
+		System.out.println("You were born in " + yearBorn + "? Wow, you're getting old.");
 		// Run the program.
-		
+
 		keyboard.close();
 	}
-
 }
--- Java/jfop/screenoutputkeyboardinput/drills/StringOutput.java
+++ Java/jfop/screenoutputkeyboardinput/drills/StringOutput.java
@@ -1,25 +1,24 @@
 
-public class StringOutput {
-
-	public static void main(String[] args) {
+public class StringOutput
+{
+	public static void main(String[] args)
+	{
 		// Examine the code and add what you expect the output to be in each comment.
 		// Then run the code.
-		
-		// Expected: 
+
+		// Expected: snow  ball
 		System.out.println("snow " + " ball");
-		
+
 		// Notice the print() statement.
-		// Expected: 
+		// Expected: "I could use some space" not in quotes, followed by two blank lines
 		System.out.print("I could use some space\n\n");
-		
+
 		int tensVal = 9;
 		int onesVal = 3;
-		// Expected: 
+		// Expected: 93
 		System.out.println("Concatenated number is " + tensVal + onesVal);
-		
-		// Expected: 
-		System.out.println(tensVal + onesVal + " is another number");
 
+		// Expected: 12
+		System.out.println(tensVal + onesVal + " is another number, " + (tensVal + onesVal) + " is the same number.");
 	}
-
 }
